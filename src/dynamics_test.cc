e1ca5bd1a2ca062578746be70a964d0348c7ec09
Nitish Thatte <nitishsthatte@gmail.com>
2021-11-08 21:27:01 -0500

    verify dynamics jacobians with finite difference in unit test

--- src/dynamics_test.cc
+++ src/dynamics_test.cc
@@ -1,6 +1,8 @@
 #include "src/dynamics.hh"
 
 #include <gtest/gtest.h>
+#include <manif/impl/se3/SE3.h>
+#include <manif/impl/se3/SE3Tangent.h>
 
 namespace src {
 TEST(Dynamics, UpdatesStateCorrectly) {
@@ -13,7 +15,7 @@ TEST(Dynamics, UpdatesStateCorrectly) {
   EXPECT_EQ(x_expected, x_new);
 }
 
-TEST(Dynamics, CalculatesCorrectStateJacobian) {
+TEST(Dynamics, CalculatesExpectedStateJacobian) {
   const auto x_orig = manif::SE3d::Identity();
   const auto u = manif::SE3d{{1.0, 0.0, 0.0}, manif::SO3d{M_PI, 0.0, 0.0}};
 
@@ -24,7 +26,27 @@ TEST(Dynamics, CalculatesCorrectStateJacobian) {
   EXPECT_EQ(J_x, J_x_expected);
 }
 
-TEST(Dynamics, CalculatesCorrectControlJacobian) {
+TEST(Dynamics, StateJacobianCloseToFiniteDifference) {
+  const auto x_orig = manif::SE3d::Identity();
+  const auto u = manif::SE3d{{1.0, 0.0, 0.0}, manif::SO3d{M_PI, 0.0, 0.0}};
+
+  manif::SE3d::Jacobian J_x = manif::SE3d::Jacobian::Zero();
+  dynamics(x_orig, u, &J_x);
+
+  const auto EPS = 1e-6;
+  for (size_t i = 0; i < StateDim; ++i) {
+    manif::SE3Tangentd delta_x = manif::SE3Tangentd::Zero();
+    delta_x[i] = EPS;
+    const manif::SE3Tangentd finit_diff_col =
+        ((x_orig + delta_x) * u - (x_orig + -1 * delta_x) * u) / (2 * EPS);
+
+    const auto error_frac =
+        (J_x.col(i) - finit_diff_col).norm() / (J_x.col(i).norm());
+    EXPECT_LT(error_frac, 0.01);
+  }
+}
+
+TEST(Dynamics, CalculatesExpectedControlJacobian) {
   const auto x_orig = manif::SE3d::Identity();
   const auto u = manif::SE3d{{1.0, 0.0, 0.0}, manif::SO3d{M_PI, 0.0, 0.0}};
 
@@ -33,5 +55,25 @@ TEST(Dynamics, CalculatesCorrectControlJacobian) {
 
   const manif::SE3d::Jacobian J_u_expected = manif::SE3d::Jacobian::Identity();
   EXPECT_EQ(J_u, J_u_expected);
+}
+
+TEST(Dynamics, ControlJacobianCloseToFiniteDifference) {
+  const auto x_orig = manif::SE3d::Identity();
+  const auto u = manif::SE3d{{1.0, 0.0, 0.0}, manif::SO3d{M_PI, 0.0, 0.0}};
+
+  manif::SE3d::Jacobian J_u = manif::SE3d::Jacobian::Zero();
+  dynamics(x_orig, u, nullptr, &J_u);
+
+  const auto EPS = 1e-6;
+  for (size_t i = 0; i < StateDim; ++i) {
+    manif::SE3Tangentd delta_u = manif::SE3Tangentd::Zero();
+    delta_u[i] = EPS;
+    const manif::SE3Tangentd finit_diff_col =
+        (x_orig * (u + delta_u) - (x_orig * (u + -1 * delta_u))) / (2 * EPS);
+
+    const auto error_frac =
+        (J_u.col(i) - finit_diff_col).norm() / (J_u.col(i).norm());
+    EXPECT_LT(error_frac, 0.01);
+  }
 }
 }  // namespace src
